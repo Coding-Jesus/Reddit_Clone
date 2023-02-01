22c0073fa4f2f7c779331b86adf4455842a1b38f
Coding-Jesus <blackjesusmp@gmail.com>
2023-02-01 18:42:59 +0000

    first commit

---
+++ reddit_clone/src/components/Navbar/RightContent/AuthButtons.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react'
+
+const AuthButtons: ReactFC = () => {
+    return (
+        <>
+            <Button>Log in</Button>
+            <Button>Sign up</Button>
+        </>
+    );
+};
+
+export default AuthButtons;
