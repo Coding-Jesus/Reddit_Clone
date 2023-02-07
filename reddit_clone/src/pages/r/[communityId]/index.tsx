e0fff194df74f3cadc576291dad6c5d9c42d9ff3
Coding-Jesus <blackjesusmp@gmail.com>
2023-02-07 12:37:05 +0000

    Add Community page server-side rendering

---
+++ reddit_clone/src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,52 @@
+import { Community } from '../../../atoms/communitiesAtom';
+import { doc, getDoc } from 'firebase/firestore';
+import { GetServerSidePropsContext } from 'next';
+import React from 'react';
+import { firestore } from "../../../firebase/clientApp";
+import safeJsonStringify from 'safe-json-stringify';
+
+type CommunityPageProps = {
+    communityData: Community;
+};
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+
+    if (!communityData) {
+        return <div>community does not exist</div>;
+    }
+
+
+    return <div>Welcome to {communityData.id}</div>;
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
+    // Get community data and pass it to client
+
+    try {
+        const communityDocRef = doc(firestore,
+            'communities',
+            context.query.communityId as string
+        );
+
+        const communityDoc = await getDoc(communityDocRef);
+
+        return {
+            props: {
+                communityData: communityDoc.exists()
+                    ? JSON.parse(
+                        safeJsonStringify({
+                            id: communityDoc.id, ...communityDoc.data()
+                        })
+                    )
+                    : "",
+            },
+        };
+
+    } catch (error) {
+        //could add error page here
+        console.log("getServerSideProps error", error);
+    }
+}
+
+export default CommunityPage;
