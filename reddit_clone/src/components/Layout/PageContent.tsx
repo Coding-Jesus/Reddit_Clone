c41f4d57b4b2d67ec5d92e255ee55ebacf6af4f4
Coding-Jesus <blackjesusmp@gmail.com>
2023-02-08 18:16:17 +0000

    Add NewPostForm 1: formTabs component

--- reddit_clone/src/components/Layout/PageContent.tsx
+++ reddit_clone/src/components/Layout/PageContent.tsx
@@ -8,18 +8,20 @@ type PageContentProps = {
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
 
     return (
-        <Flex justifyContent="center" p="16px 0px" border="1px solid red">
+        <Flex
+            justifyContent="center"
+            p="16px 0px"
+        >
             <Flex
                 width="95%"
                 justify="center"
                 maxWidth="860px"
-                border="1px solid green">
+            >
                 {/* LHS */}
                 <Flex
                     direction="column"
                     width={{ base: "100%", md: "65%" }}
                     mr={{ base: 0, md: 6 }}
-                    border="1px solid blue"
                 >
                     {children && children[0 as keyof typeof children]}
                 </Flex>
@@ -29,7 +31,7 @@ const PageContent: React.FC<PageContentProps> = ({ children }) => {
                     direction="column"
                     width={{ base: "none", md: "flex" }}
                     flexGrow={1}
-                    border="1px solid orange">
+                >
                     {children && children[1 as keyof typeof children]}
                 </Flex>
             </Flex>
--- reddit_clone/src/pages/r/[communityId]/submit.tsx
+++ reddit_clone/src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,26 @@
+import PageContent from '../../../components/Layout/PageContent';
+import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import NewPostForm from '../../../components/Posts/NewPostForm';
 
 const submit: React.FC = () => {
 
-    return <div>Submit page</div>
+    return (
+        <PageContent>
+            <>
+                <Box
+                    p="14px 0px"
+                    borderBottom="1px soild"
+                    borderColor="white"
+                >
+                    <Text>Create a post</Text>
+                </Box>
+                <NewPostForm />
+            </>
+            <>
+                {/* About */}
+            </>
+        </PageContent>
+    )
 }
 export default submit;
