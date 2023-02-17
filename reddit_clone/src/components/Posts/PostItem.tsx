eb99c94f94269f51c52797f3088c4449f60ddfe1
Coding-Jesus <blackjesusmp@gmail.com>
2023-02-17 20:54:41 +0000

    Add Single post page(Next.js dynamic [pid] route, event prorpagation)

--- reddit_clone/src/components/Posts/PostItem.tsx
+++ reddit_clone/src/components/Posts/PostItem.tsx
@@ -1,6 +1,7 @@
 import { Post } from '@/atoms/postsAtom';
 import { Flex, Icon, Stack, Text, Image, Skeleton, Spinner, Alert, AlertIcon } from '@chakra-ui/react';
 import moment from 'moment';
+import { useRouter } from 'next/router';
 import React, { useState } from 'react';
 import { AiOutlineDelete } from "react-icons/ai";
 import { BsChat, BsDot } from "react-icons/bs";
@@ -18,9 +19,14 @@ type PostItemProps = {
     post: Post;
     userIsCreator: boolean;
     userVoteValue?: number;
-    onVote: (post: Post, vote: number, communityId: string) => void;
+    onVote: (
+        event: React.MouseEvent<SVGElement, MouseEvent>,
+        post: Post,
+        vote: number,
+        communityId: string) => void;
+
     onDeletePost: (post: Post) => Promise<boolean>;
-    onSelectsPost: () => void;
+    onSelectsPost?: (post: Post) => void;
 };
 
 const PostItem: React.FC<PostItemProps> = ({
@@ -34,10 +40,13 @@ const PostItem: React.FC<PostItemProps> = ({
 
     const [loadingImage, setLoadingImage] = useState(true);
     const [loadingDelete, setLoadingDelete] = useState(false);
+    const singlePostPage = !onSelectsPost
+    const rotuer = useRouter();
     const [error, setError] = useState(false);
 
 
-    const handleDelete = async () => {
+    const handleDelete = async (event: React.MouseEvent<HTMLDivElement, MouseEvent>,) => {
+        event.stopPropagation();
         setLoadingDelete(true);
         try {
             const success = await onDeletePost(post);
@@ -46,7 +55,9 @@ const PostItem: React.FC<PostItemProps> = ({
                 throw new Error("Failed to delete post");
             }
             console.log("Post was successfully deleted")
-
+            if (singlePostPage) {
+                rotuer.push(`/r/${post.communityId}`);
+            }
         } catch (error: any) {
             setError(error.message);
         }
@@ -57,24 +68,24 @@ const PostItem: React.FC<PostItemProps> = ({
         <Flex
             border="1px solid"
             bg="white"
-            borderColor="gray.300"
-            borderRadius={4}
-            _hover={{ borderColor: "gray.500" }}
-            cursor="pointer"
-            onClick={onSelectsPost}
+            borderColor={singlePostPage ? "white" : "gray.300"}
+            borderRadius={singlePostPage ? "4px 4px 0px 0px" : "4px"}
+            _hover={{ borderColor: singlePostPage ? "none" : "gray.500" }}
+            cursor={singlePostPage ? " unset" : "pointer"}
+            onClick={() => onSelectsPost && onSelectsPost(post)}
         >
             <Flex
                 direction="column"
                 align="center"
-                bg="gray.100"
+                bg={singlePostPage ? "none" : "gray.100"}
                 p={2}
                 width="40px"
-                borderRadius={4}
+                borderRadius={singlePostPage ? "0" : "3px 0px 0px 3px"}
             >
                 <Icon as={userVoteValue === 1 ? IoArrowUpCircleSharp : IoArrowUpCircleOutline}
                     color={userVoteValue === 1 ? "brand.100" : "gray.400"}
                     fontSize={22}
-                    onClick={() => onVote(post, 1, post.communityId)}
+                    onClick={(event) => onVote(event, post, 1, post.communityId)}
                     cursor="pointer"
                 />
 
@@ -83,7 +94,7 @@ const PostItem: React.FC<PostItemProps> = ({
                 <Icon as={userVoteValue === -1 ? IoArrowDownCircleSharp : IoArrowDownCircleOutline}
                     color={userVoteValue === -1 ? "#4379ff" : "gray.400"}
                     fontSize={22}
-                    onClick={() => onVote(post, -1, post.communityId)}
+                    onClick={(event) => onVote(event, post, -1, post.communityId)}
                     cursor="pointer"
                 />
             </Flex>
--- reddit_clone/src/hooks/usePosts.tsx
+++ reddit_clone/src/hooks/usePosts.tsx
@@ -7,18 +7,22 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { communityState } from '@/atoms/communitiesAtom';
 import { useEffect } from 'react';
 import { AuthModalState } from '@/atoms/authModalAtom';
+import { useRouter } from 'next/router';
 
 const usePosts = () => {
+    const router = useRouter();
     const [user] = useAuthState(auth);
     const [postStateValue, setPostStateValue] = useRecoilState(postState);
     const currentCommunity = useRecoilValue(communityState).currentCommunity;
     const setAuthModalState = useSetRecoilState(AuthModalState);
 
     const onVote = async (
+        event: React.MouseEvent<SVGElement, MouseEvent>,
         post: Post,
         vote: number,
         communityId: string,) => {
 
+        event.stopPropagation();
         // check for a user => if not open auth modal
         if (!user?.uid) {
             setAuthModalState({ open: true, view: "login" });
@@ -99,12 +103,6 @@ const usePosts = () => {
                     voteChange = 2 * vote;
                 }
             }
-
-            const postRef = doc(firestore, "posts", post.id);
-            batch.update(postRef, { voteStatus: voteStatus + voteChange });
-
-            await batch.commit();
-
             // update state with updated values
             const postIdx = postStateValue.posts.findIndex(
                 (item) => item.id === post.id
@@ -116,13 +114,31 @@ const usePosts = () => {
                 postVotes: updatedPostVotes,
             }));
 
+            if (postStateValue.selectedPost) {
+                setPostStateValue((prev) => ({
+                    ...prev, selectedPost: updatedPost,
+                }))
+            }
+
+            const postRef = doc(firestore, "posts", post.id);
+            batch.update(postRef, { voteStatus: voteStatus + voteChange });
+
+            await batch.commit();
+
+
         } catch (error: any) {
             console.log("onVote error", error.message)
         }
     };
 
 
-    const onSelectPost = () => { };
+    const onSelectPost = (post: Post) => {
+        setPostStateValue((prev) => ({
+            ...prev,
+            selectedPost: post,
+        }))
+        router.push(`/r/${post.communityId}/comments/${post.id}`);
+    };
 
     const onDeletePost = async (post: Post): Promise<boolean> => {
         try {
